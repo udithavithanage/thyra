a7a33d2e7dd148fb288101e6d4edba93d2f47d13
Sithija Nelusha Silva <hello@snelusha.dev>
2025-11-26 10:00:08 +0530

    Read version from package.json directly

--- src/commands/version.js
+++ src/commands/version.js
@@ -1,12 +1,5 @@
-import fs from "node:fs";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
+import { version } from "../../package.json" assert { type: "json" };
 
 export function runVersion() {
-  const __filename = fileURLToPath(import.meta.url);
-  const __dirname = path.dirname(__filename);
-  const packagePath = path.join(__dirname, "../../package.json");
-
-  const data = JSON.parse(fs.readFileSync(packagePath, "utf8"));
-  console.log(`v${data.version}`);
+  console.log(`v${version}`);
 }
