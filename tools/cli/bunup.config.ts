b837a5fd7e52d543d4956a52f38807eeef6c02ba
uditha_vithanage <udithavithanage358@gmail.com>
2026-08-26 13:58:17 +0530

    feat: externalize node modules in bunup configuration and add peerDependencies in package.json

--- tools/cli/bunup.config.ts
+++ tools/cli/bunup.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from "bunup";
 
-import { version } from "package" with { type: "json" };
+import {
+  version,
+  dependencies,
+  peerDependencies,
+} from "package" with { type: "json" };
 
 export default defineConfig({
   entry: ["src/index.ts"],
+  external: [...Object.keys(dependencies), ...Object.keys(peerDependencies)],
   minify: true,
-  banner: "#!/usr/bin/env node",
+  banner: "#!/usr/bin/env node\n",
   footer: "// Smart navigation for developers who value time.",
   env: {
     NODE_ENV: "production",
